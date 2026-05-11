3f6b2fb90810e5ac1ef78dfff0403cffb420b7e0
arina <arinademikhova@gmail.com>
2026-05-11 12:42:13 +0300

    nachalo

---
+++ main.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="Аналитика Эко-парк Адмирал", layout="wide")
+st.markdown('<h1 class="main-header">🏕 Эко-парк "Адмирал"</h1>', unsafe_allow_html=True)
+st.write("Пока пусто")
